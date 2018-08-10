aab6d113946621ade87f144f796b0c633e61785b
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-08-11 00:07:03 +0530

    installed pug and created my first view using pug templating engine

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
 app.get('/', function(req, res){
-  res.send('Hello world');
+  res.render('index', {
+    title: 'Hello'
+  });
 })
 
 app.listen(3000, function(){
