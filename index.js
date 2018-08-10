9e90379c5eb06535944308dc071b47425cf769f2
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-08-11 00:47:28 +0530

    created my first db and use that in program

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db  = mongoose.connection;
+
+db.once('open', function(){
+  console.log('connected to mongodb');
+});
+
+db.on('error', function(err){
+  console.log(err);
+
+});
+
+
 
 const app = express();
+
+let Article = require('./models/article');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
 app.get('/', function(req, res){
-  let articles = [
-    {
-    id : 1,
-    title: 'Article One',
-    author: 'Brad Traversy',
-    body: 'This is article one'
-  },
-  {
-    id:2,
-    title: 'Article Two',
-    author: 'John Doe',
-    body:'This is article two'
-  },
-  {
-    id:3,
-    title: 'Article Three',
-    author: 'Brad Traversy',
-    body:'This is article three'
-  },
-  ]
-  res.render('index', {
-    title: 'Hello',
-    articles: articles
+  Article.find({}, function(err, articles){
+    if(err){
+        console.log(err)}
+        else{
+          res.render('index', {
+            title: 'Articles',
+            articles: articles
+          });
+        }
+    });
   });
-})
+
 
 app.get('/articles/add',function(req, res){
   res.render('add_article', {
