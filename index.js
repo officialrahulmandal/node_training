6b67272a4bc7e01f8e9b4fbead1345c6030bdcfd
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-08-11 00:27:22 +0530

    added base template and use for loop for displaying the content

--- index.js
+++ index.js
@@ -8,10 +8,38 @@ app.set('view engine', 'pug');
 
 
 app.get('/', function(req, res){
+  let articles = [
+    {
+    id : 1,
+    title: 'Article One',
+    author: 'Brad Traversy',
+    body: 'This is article one'
+  },
+  {
+    id:2,
+    title: 'Article Two',
+    author: 'John Doe',
+    body:'This is article two'
+  },
+  {
+    id:3,
+    title: 'Article Three',
+    author: 'Brad Traversy',
+    body:'This is article three'
+  },
+  ]
   res.render('index', {
-    title: 'Hello'
+    title: 'Hello',
+    articles: articles
   });
 })
+
+app.get('/articles/add',function(req, res){
+  res.render('add_article', {
+    title:'Add Article'
+  });
+});
+
 
 app.listen(3000, function(){
   console.log("Listning to port 3000")
