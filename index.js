eb084a585b97e6c40c7cb91ec6d8f421bd70622b
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-08-11 10:38:12 +0530

    successfully created an interface to add new article

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db  = mongoose.connection;
@@ -23,6 +24,9 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: false}))
+app.set('view engine', 'pug');
+
 
 app.get('/', function(req, res){
   Article.find({}, function(err, articles){
@@ -43,7 +47,21 @@ app.get('/articles/add',function(req, res){
     title:'Add Article'
   });
 });
+app.post('/articles/add',  function(req, res){
+  let  article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;;
 
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/')
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("Listning to port 3000")
